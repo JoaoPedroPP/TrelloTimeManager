c334121a60c5cfb8e73f4371bf062736060ca425
Joao Pedro <jp052@hotmail.com>
2019-02-09 10:21:40 -0200

    update config service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -20,6 +20,7 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.configService.getTrelloKey();
     this.configService.auth.subscribe(data => {if(data === true)this.router.navigate(['logged', 'home']);});
   }
 
--- src/app/services/configs.service.ts
+++ src/app/services/configs.service.ts
@@ -8,26 +8,18 @@ export class ConfigsService {
   trelloKey: string;
   auth= new EventEmitter<boolean>();
 
-  constructor(private electronService: ElectronService) {
-    this.electronService.ipcRenderer.on('key:set', (event, key) => {
-      console.log(key);
-      this.trelloKey = key;
-      this.auth.emit(true);
-    });
-    this.getTrelloKey();
-    this.electronService.ipcRenderer.on('key:get:response', (event, data) => {
-      if(data != ''){
-        this.trelloKey = data;
-        this.auth.emit(true);
-      }
-      else this.auth.emit(false);
-    });
-  }
+  constructor(private electronService: ElectronService) { }
 
   setTrelloKey(form: string) {
     this.electronService.ipcRenderer.send("setNewAPIKey", form);
+    this.electronService.ipcRenderer.on('key:set', (event, data) => {
+      data === form ? this.auth.emit(true):this.auth.emit(false);
+    });
   }
   getTrelloKey() {
     this.electronService.ipcRenderer.send('key:get');
+    this.electronService.ipcRenderer.on('key:get:response', (event, data) => {
+      data !== '' ? this.auth.emit(true):this.auth.emit(false);
+    });
   }
 }
